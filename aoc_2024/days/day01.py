cc2e741dbfb3803c76384c591d1948fad0c84987
Karin Touchie <ktouchie@gmail.com>
2024-12-01 17:10:15 +0000

    Day 1: Part 2

--- aoc_2024/days/day01.py
+++ aoc_2024/days/day01.py
@@ -1,32 +1,69 @@
+import logging
 import sys
 
 import pandas as pd
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-class Day01:
-    def __init__(self, input_file):
-        self.input_file = input_file
 
-    def calculate_differences_sum(self):
-        # Load the data into a pandas DataFrame
-        df = pd.read_csv(
-            self.input_file, sep=r"\s+", header=None, names=["col1", "col2"]
-        )
+def calculate_differences_sum(input_file):
+    # Load the data into a pandas DataFrame
+    df = pd.read_csv(input_file, sep=r"\s+", header=None, names=["col1", "col2"])
 
-        # Sort each column
-        sorted_col1 = df["col1"].sort_values().reset_index(drop=True)
-        sorted_col2 = df["col2"].sort_values().reset_index(drop=True)
+    # Sort each column
+    sorted_col1 = df["col1"].sort_values().reset_index(drop=True)
+    sorted_col2 = df["col2"].sort_values().reset_index(drop=True)
 
-        # Calculate the differences between the corresponding sorted values
-        differences = abs(sorted_col1 - sorted_col2)
+    # Calculate the differences between the corresponding sorted values
+    differences = abs(sorted_col1 - sorted_col2)
 
-        # Calculate the sum of all differences
-        total_difference = differences.sum()
+    # Calculate the sum of all differences
+    total_difference = differences.sum()
 
-        # Display the results
-        print(differences)
-        print(f"Sum of all differences: {total_difference}")
-        return total_difference
+    # Display the results
+    logger.info(f"Sum of all differences: {total_difference}")
+    return sorted_col1, sorted_col2, total_difference
+
+
+def calculate_similarity_score(sorted_col1, sorted_col2):
+    # Initialize similarity score
+    similarity_score = 0
+
+    # Count occurrences of each number in 2nd column
+    counts_col2 = _count_occurrences(sorted_col2)
+
+    # Calculate similarity score based on common elements
+    for i in sorted_col1:
+        if i in counts_col2:
+            similarity_score += i * counts_col2[i]
+
+    # Log the similarity score
+    logger.info(f"Similarity score: {similarity_score}")
+    return similarity_score
+
+
+def _count_occurrences(sorted_column):
+    # Initialize counts dictionary
+    counts = {}
+
+    # Initialize current number and count
+    current_num = sorted_column[0]
+    current_count = 1
+
+    # Count occurrences of each number in the sorted column
+    for n in sorted_column[1:]:
+        if n == current_num:
+            current_count += 1
+        else:
+            counts[current_num] = current_count
+            current_num = n
+            current_count = 1
+
+    # Add the last counted number to the dictionary
+    counts[current_num] = current_count
+
+    return counts
 
 
 if __name__ == "__main__":
@@ -35,5 +72,5 @@ if __name__ == "__main__":
         sys.exit(1)
 
     input_file = sys.argv[1]
-    day01 = Day01(input_file)
-    day01.calculate_differences_sum()
+    sorted_col1, sorted_col2, total_difference = calculate_differences_sum(input_file)
+    calculate_similarity_score(sorted_col1, sorted_col2)
--- aoc_2024/tests/test_day01.py
+++ aoc_2024/tests/test_day01.py
@@ -2,9 +2,14 @@ import logging
 
 import pytest
 
-from aoc_2024.days.day01 import Day01
+from aoc_2024.days.day01 import (
+    _count_occurrences,
+    calculate_differences_sum,
+    calculate_similarity_score,
+)
 from aoc_2024.utils import get_input_file_path
 
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
@@ -15,5 +20,20 @@ class TestDay01:
         [get_input_file_path("01_part1.txt")],
     )
     def test_calculate_differences_sum(self, input_file):
-        day01 = Day01(input_file)
-        assert day01.calculate_differences_sum() == 11
+        _, _, differences_sum = calculate_differences_sum(input_file)
+        assert differences_sum == 11
+
+    @pytest.mark.parametrize(
+        "sorted_column, expected_output",
+        [
+            ([1, 2, 3, 3, 3, 4], {1: 1, 2: 1, 3: 3, 4: 1}),
+            ([3, 3, 3, 4, 5, 9], {3: 3, 4: 1, 5: 1, 9: 1}),
+        ],
+    )
+    def test_count_occurrences(self, sorted_column, expected_output):
+        assert _count_occurrences(sorted_column) == expected_output
+
+    def test_calculate_similarity_score(self):
+        sorted_col1 = [1, 2, 3, 3, 3, 4]
+        sorted_col2 = [3, 3, 3, 4, 5, 9]
+        assert calculate_similarity_score(sorted_col1, sorted_col2) == 31
