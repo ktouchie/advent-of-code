8f9288a7bbe4269d727c7642b3d466617ef0c5be
Karin Touchie <ktouchie@gmail.com>
2024-12-03 08:42:58 +0000

    Day 3: Part 1

---
+++ aoc_2024/days/day03.py
@@ -0,0 +1,45 @@
+import logging
+import re
+import sys
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+mul_pattern = r"(?:mul(\(\d{1,3}\,\d{1,3}\)))"
+
+
+def get_matches_from_file(input_file):
+    # Read the input file and return a list of regex matches
+    with open(input_file, "r") as f:
+        content = f.read()
+    print(content)
+    mul_matches = re.findall(mul_pattern, content)
+    logger.info(mul_matches)
+    logger.info(f"Number of matches: {len(mul_matches)}")
+    return mul_matches
+
+
+def get_sum_of_match_products(mul_matches):
+    # Initialize sum of products
+    sum_of_products = 0
+    # Get lists from matches and calculate the product
+    for match in mul_matches:
+        match_list = match[1:-1].split(",")
+        sum_of_products += mul(match_list)
+    # Log the sum of products
+    logger.info(f"Sum of products: {sum_of_products}")
+    return sum_of_products
+
+
+def mul(match_list):
+    return int(match_list[0]) * int(match_list[1])
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python day03.py <input_file>")
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+    matches = get_matches_from_file(input_file)
+    get_sum_of_match_products(matches)
---
+++ aoc_2024/tests/test_day03_part1.py
@@ -0,0 +1,33 @@
+import logging
+
+import pytest
+
+from aoc_2024.days.day03 import get_matches_from_file, get_sum_of_match_products, mul
+from aoc_2024.utils import get_input_file_path
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class TestDay02:
+
+    @pytest.mark.parametrize(
+        "input_file",
+        [get_input_file_path("03_part1.txt")],
+    )
+    def test_get_matches_from_file(self, input_file):
+        assert get_matches_from_file(input_file) == [
+            "(2,4)",
+            "(5,5)",
+            "(11,8)",
+            "(8,5)",
+        ]
+
+    def test_get_sum_of_match_products(self):
+        assert get_sum_of_match_products(["(2,4)", "(5,5)", "(11,8)", "(8,5)"]) == 161
+
+    def test_mul(self):
+        assert mul(["2", "4"]) == 8
+        assert mul(["5", "5"]) == 25
+        assert mul(["11", "8"]) == 88
+        assert mul(["8", "5"]) == 40
